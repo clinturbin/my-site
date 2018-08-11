f9c87a66f36edc3b8e462eec9b57540ee966ed7a
clinturbin <clinturbin@gmail.com>
2018-08-11 15:54:38 -0400

    Redesigned header and changed colors

--- main.js
+++
@@ -1,7 +0,0 @@
-var menuIcon = document.querySelector('.menu-icon');
-var navigationItems = document.querySelector('.header-right');
-
-menuIcon.addEventListener('click', function() {
-    navigationItems.classList.toggle('hidden');
-    navigationItems.classList.toggle('visible');
-});

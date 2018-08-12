122572be8d3069641db7dc6483a8a43b73e6ffb8
clinturbin <clinturbin@gmail.com>
2018-08-12 12:38:58 -0400

    Added arrow to expand and collapse section content

---
+++ main.js
@@ -0,0 +1,9 @@
+var expandCollapseArrows = document.querySelectorAll(".section-arrow");
+
+expandCollapseArrows.forEach(function (arrow) {
+    arrow.addEventListener("click", function () {
+        this.classList.toggle("expand-button");
+        this.classList.toggle("collapse-button");
+        this.parentNode.nextElementSibling.classList.toggle("hide");
+    })
+})

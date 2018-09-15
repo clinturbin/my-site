bd64b2084062b5f6cac579efc6c909c2157d6233
clinturbin <clinturbin@gmail.com>
2018-09-15 11:59:32 -0400

    Added inidividual project modal screen for mobile display

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 const menuIcon = document.querySelector('.menu-icon');
 const flyOutNavigation = document.querySelector('.flyout-navigation');
+const projectImages = document.querySelectorAll('.project-image');
+const modalScreens = document.querySelectorAll('.project-info-modal');
+const projectLinkButtons = document.querySelectorAll('.project-link-button');
+
+for (let image of projectImages) {
+    image.addEventListener('click', () => {
+        let modal = image.nextElementSibling;
+        modal.classList.remove('hidden-modal');
+        modal.classList.add('visible-modal');
+    })
+};
+
+for (let modal of modalScreens) {
+    modal.addEventListener('click', () => {
+        modal.classList.remove('visible-modal');
+        modal.classList.add('hidden-modal');
+    })
+};
+
+for (let button of projectLinkButtons) {
+    button.addEventListener('click', (event) => {
+        event.stopPropagation();
+    })
+};
 
 menuIcon.addEventListener('click', () => {
     flyOutNavigation.classList.toggle('hidden');
